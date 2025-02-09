6bc45dd784f226c956b3fd07821807176ca19fbd
YakovlevDanylo <166537891+YakovlevDanylo@users.noreply.github.com>
2025-02-09 12:07:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,17 +13,20 @@ class GameSprite(sprite.Sprite):
         screen.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
+
     def update(self, screen):
         keys = key.get_pressed()
         if keys[K_a] and self.rect.x > 0:
             self.rect.x -= self.speed
         if keys[K_d] and self.rect.x < 630:
             self.rect.x += self.speed
-        if keys[K_w] and self.rect.y > 0:
-            self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < 430:
+        if keys[K_s] and self.rect.y > 0:
             self.rect.y += self.speed
+        if keys[K_w] and self.rect.x < 430:
+            self.rect.y -= self.speed
+
         super().update(screen)
+
 
 window = display.set_mode((700, 500))
 display.set_caption("Лабіринт")
@@ -31,11 +34,12 @@ display.set_caption("Лабіринт")
 background = transform.scale(image.load("background.jpg"), (700, 500))
 player = Player("cat.jpg", 100, 400, 70, 70, 5)
 
+
 timer = time.Clock()
 fps = 60
 
-finished = False
 end_game = False
+finished = False
 
 while not end_game:
     for e in event.get():
@@ -43,9 +47,10 @@ while not end_game:
             end_game = True
 
     if not finished:
-        window.blit(background, (0, 0))
+        window.blit(background, (0,0))
 
         player.update(window)
+
 
         display.update()
         timer.tick(fps)
